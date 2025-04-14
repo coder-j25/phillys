df8cf76e2b1c8cdca2ded85c4b7def90958ddd6b
Anthony Bosek <anthony.bosek@gmail.com>
2025-04-14 11:06:35 -0700

    test commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
- 
-
   return (
     <>
-    
+      <h1>This is a test element</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
