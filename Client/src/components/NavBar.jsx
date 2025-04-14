fd2938faead140f0f74eb0a5606829b81d827f45
coder-j25 <justinee.hanson@outlook.com>
2025-04-14 11:34:12 -0700

    NavBar start

---
+++ Client/src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import logo from "./../assets/logo.jpg"
+import React from "react";
+
+
+const NavBar = () => {
+    return (
+        <>
+            <div>
+                <div>
+                   <img src={logo} className="w-[15px]"/>
+                </div>
+<div>
+   
+</div>
+            </div>
+        </>
+    )
+}
+
+export default NavBar
