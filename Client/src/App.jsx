2e652c3e2aef184dac692550e7b6b9bd9b63f6da
Rachael <rma199022@gmail.com>
2025-04-15 10:42:23 -0500

    created imports for diff views

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,18 @@
 import './index.css'
 import { Routes, Route } from "react-router-dom";
+// import AboutMe from './views/AboutMe';
+// import Menu from './views/Menu';
+// import FindUs from './views/FindUs';
+// import ContactUs from './views/ContactUs';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<AboutMe />}/>
-        <Route path="/menu" element={<Menu />}/>
-        <Route path="/findus" element={<FindUs />}/>
-        <Route path="/contact" element={<ContactUs />}/>
+        {/* <Route path="/" element={<AboutMe />}/> */}
+        {/* <Route path="/menu" element={<Menu />}/> */}
+        {/* <Route path="/findus" element={<FindUs />}/> */}
+        {/* <Route path="/contact" element={<ContactUs />}/> */}
       </Routes>
     </>
   );
