7a132f985ef692ab1bcb5a5c9a92d161119a3005
CrisTollinchi <cristollinchi@gmail>
2025-04-17 09:22:19 -0400

    Updated the AboutMe component

--- Client/src/views/AboutMe.jsx
+++ Client/src/views/AboutMe.jsx
@@ -1,14 +1,16 @@
 const AboutMe = () => {
   return (
-    <div className="bg-black text-white h-screen p-3">
-      <div className="flex flex-col gap-2 w-80 p-3">
-      <h1 className="text-6xl">Willy's Philly's</h1>
-      <p>
-        Serving the best Philly cheesesteaks in the Tucson area! Visit our Find
-        us page to check our calendar and see where we'll be next.
-      </p>
+    <div className="flex flex-col-reverse relative bg-[url('/src/assets/hero.jpg')] bg-no-repeat bg-cover text-white h-screen p-3">
+      <div className="absolute inset-0 bg-black/50 z-0"></div>
+      <div className="absolute z-10 flex flex-col gap-2 w-80 p-3 lg:mx-56 my-56">
+        <h1 className="text-6xl font-bold">Willy's Philly's</h1>
+        <p className="font-semibold">
+          Serving the best Philly cheesesteaks in the Tucson area! Visit our
+          Find us page to check our calendar and see where we'll be next.
+        </p>
+        <button className="bg-black border-2 border-white">Find Us</button>
       </div>
-        {/* section for placeholder  */}
+      {/* section for placeholder  */}
       <div>
         <p>
           Placeholder section for additional homepage content that will be
